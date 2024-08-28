fa11748b3b1afed829b66966edbfa4c350b77bfe
cirroskais <cirroskais@madhouselabs.net>
2024-08-28 03:39:16 -0400

    store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,4 +1,4 @@
 import { writable, type Writable } from "svelte/store";
 
-export type Page = "home" | "members" | "services";
+export type Page = "home" | "members" | "services" | "missing";
 export let pageStore: Writable<Page> = writable("home");
